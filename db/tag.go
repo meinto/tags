c793e1dd0ab68df479e3c02d45b647e2157ab9d3
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2018-09-02 06:01:10 +0200

    Implement tag creation

--- db/tag.go
+++ db/tag.go
@@ -6,11 +6,16 @@ import (
 
 type Tag struct {
 	gorm.Model
+	Tag string
 }
 
-func (db *DB) CreateTag() *Tag {
-	// TODO
-	return &Tag{}
+func (db *DB) CreateTag(str string) Tag {
+	db.instance.Create(&Tag{
+		Tag: str,
+	})
+	var tag Tag
+	db.instance.Last(&tag, "tag = ?", str)
+	return tag
 }
 
 func (db *DB) CountTags() int {
--- server/routes/tags.go
+++ server/routes/tags.go
@@ -1,6 +1,9 @@
 package routes
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
@@ -11,5 +14,16 @@ func (rs *Routes) CountTags(w http.ResponseWriter, r *http.Request, _ httprouter
 }
 
 func (rs *Routes) CreateTag(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	body := r.Body
+	defer body.Close()
 
+	bodyBytes, err := ioutil.ReadAll(body)
+	if err != nil {
+		w.Header().Add("status", fmt.Sprintf("%d", http.StatusInternalServerError))
+		w.Write([]byte("error occured while reading body :("))
+	}
+	tag := rs.db.CreateTag(string(bodyBytes))
+
+	enc := json.NewEncoder(w)
+	enc.Encode(tag)
 }
