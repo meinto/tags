48f8a5cab8ff81fce359c22cdb00b787464db341
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2018-09-02 06:36:03 +0200

    Implement count function

--- db/tag.go
+++ db/tag.go
@@ -1,6 +1,9 @@
 package db
 
 import (
+	"encoding/json"
+	"strings"
+
 	"github.com/jinzhu/gorm"
 )
 
@@ -18,7 +21,31 @@ func (db *DB) CreateTag(str string) Tag {
 	return tag
 }
 
-func (db *DB) CountTags() int {
-	// TODO
-	return 0
+func (db *DB) CountTags(needle map[string]interface{}) int {
+	rows, err := db.instance.Table("tags").Select("*").Rows()
+	defer rows.Close()
+	if err != nil {
+		return -1
+	}
+
+	count := 0
+	for rows.Next() {
+		found := true
+		var tag Tag
+		db.instance.ScanRows(rows, &tag)
+
+		var haystack map[string]interface{}
+		json.NewDecoder(strings.NewReader(tag.Tag)).Decode(&haystack)
+
+		for k := range needle {
+			if needle[k] != haystack[k] {
+				found = false
+			}
+		}
+
+		if found {
+			count++
+		}
+	}
+	return count
 }
--- server/routes/tags.go
+++ server/routes/tags.go
@@ -9,21 +9,34 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func (rs *Routes) CountTags(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-
-}
-
 func (rs *Routes) CreateTag(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	body := r.Body
 	defer body.Close()
 
 	bodyBytes, err := ioutil.ReadAll(body)
 	if err != nil {
-		w.Header().Add("status", fmt.Sprintf("%d", http.StatusInternalServerError))
+		w.Header().Add("Status", fmt.Sprintf("%d", http.StatusInternalServerError))
 		w.Write([]byte("error occured while reading body :("))
+		return
 	}
 	tag := rs.db.CreateTag(string(bodyBytes))
 
 	enc := json.NewEncoder(w)
 	enc.Encode(tag)
+}
+
+func (rs *Routes) CountTags(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	body := r.Body
+	defer body.Close()
+
+	var jsn map[string]interface{}
+	if err := json.NewDecoder(body).Decode(&jsn); err != nil {
+		w.Header().Add("Status", fmt.Sprintf("%d", http.StatusInternalServerError))
+		w.Write([]byte("error occured while decoding body :("))
+		return
+	}
+	nmbr := rs.db.CountTags(jsn)
+
+	enc := json.NewEncoder(w)
+	enc.Encode(map[string]int{"count": nmbr})
 }
